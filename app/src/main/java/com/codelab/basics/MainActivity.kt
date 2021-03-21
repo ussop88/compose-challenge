545ea0e82a1b709ee09047c84f9ea19151235a38
Youssef MOURCHID <mourchid.youssef@gmail.com>
2021-03-21 18:36:22 +0100

    handle modifiers

--- app/src/main/java/com/codelab/basics/MainActivity.kt
+++ app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -1,14 +1,64 @@
 package com.codelab.basics
 
+import android.content.ContentProvider
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.compose.animation.core.animateIntSizeAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            PhotographedCard()
+        }
+    }
+
+    @Preview
+    @Composable
+    private fun PhotographedCard() {
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+                .clip(RoundedCornerShape(4.dp))
+                .background(MaterialTheme.colors.surface)
+                .clickable {}
+                .padding(20.dp),
+        ) {
+            Surface(
+                modifier = Modifier.size(45.dp),
+                shape = CircleShape,
+                color = MaterialTheme.colors.onSurface.copy(0.4f)
+            ) {
+
+            }
+            Column(
+                modifier = Modifier
+                    .padding(start = 5.dp)
+            ) {
+                Text("Alfred Sysley", fontWeight = FontWeight.Bold)
+                CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                    Text(
+                        "3 minutes ago", style = MaterialTheme.typography.body2
+                    )
+                }
+            }
         }
     }
 }
